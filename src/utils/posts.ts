b44ee581513410e4791945f75a3fb49982dcee0f
Ada Wong <wong.ada1018@gmail.com>
2025-12-10 20:02:05 -0500

    write helper function to read from any markdown post directories

---
+++ src/utils/posts.ts
@@ -0,0 +1,37 @@
+// src/utils/posts.ts
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+// helper fn for reading Markdown directory
+function readMarkdownDir(directory: string) {
+  const fileNames = fs.readdirSync(directory);
+
+  const posts = fileNames.map((fileName) => {
+    const slug = fileName.replace(/\.md$/, "");   // remove .md extension
+    const fullPath = path.join(directory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const { data, content } = matter(fileContents);
+
+    return {
+      slug,
+      frontmatter: data,
+      content,
+    };
+  });
+
+  return posts.sort(
+    (a, b) => new Date(b.frontmatter.date).getTime() - new Date(a.frontmatter.date).getTime()
+  );
+}
+
+export function getAllBlogPosts() {
+  const blogDir = path.join(process.cwd(), "src/data/blog");
+  return readMarkdownDir(blogDir);
+}
+
+export function getAllProjectPosts() {
+  const projectDir = path.join(process.cwd(), "src/data/projects");
+  return readMarkdownDir(projectDir);
+}
+
