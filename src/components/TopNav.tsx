6267a49206c937de44d13b21adaa54d8d8231a6d
Ada Wong <wong.ada1018@gmail.com>
2026-01-11 21:28:15 +0800

    finalize top nav bar

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,33 +1,37 @@
 "use client";
 
-import Link from "next/link";
-import { EnvelopeSimpleIcon, GithubLogoIcon, HouseIcon } from "@phosphor-icons/react";
-
 export default function TopNav() {
   return (
-    <div className="flex gap-2 my-2">
-      <a href="/" aria-label="Home">
-        <HouseIcon size={32} />
-      </a>
-
-      <a href="mailto:business.adawong@gmail.com" 
-      aria-label="Email"
-      className=""
-      >
-        <EnvelopeSimpleIcon size={32} />
-      </a>
-
-      <a
-        href="https://github.com/adawongHL"
-        aria-label="GitHub"
-        target="_blank"
-        rel="noopener noreferrer"
-        className=""
-      >
-        <GithubLogoIcon size={32} />
-      </a>
+    <div className="flex gap-6 my-8 h-12 items-center font-monda sticky top-0 bg-background">
+      {[
+        { label: "Home", href: "/" },
+        { label: "Email", href: "mailto:business.adawong@gmail.com" },
+        { label: "Github", href: "https://github.com/adawongHL" },
+      ].map(({ label, href }) => (
+        <a
+          key={label}
+          href={href}
+          className="group"
+        >
+          <span className="relative inline-block">
+            {/* hover underline */}
+            <span
+              className="
+                pointer-events-none
+                absolute left-0 bottom-0
+                h-[2px] w-0
+                bg-primary
+                transition-all duration-150 ease-out
+                group-hover:w-full
+                left-1/2 -translate-x-1/2
+              "
+            />
+            <span className="transition-colors duration-150 group-hover:text-primary font-bold">
+              {label}
+            </span>
+          </span>
+        </a>
+      ))}
     </div>
-
-      
   );
 }
