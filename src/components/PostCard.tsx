a20bfb5c4be0f7559ee2a498ce428900e9996887
Ada Wong <wong.ada1018@gmail.com>
2026-01-11 22:14:08 +0800

    change font on Post card

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -38,7 +38,7 @@ export default function PostCard({ title, description, image, href }: PostProps)
             className="mb-4 w-full h-48 object-cover rounded"
           />
         )}
-        <h3 className="text-xl font-bold mb-2">{title}</h3>
+        <h3 className="text-xl font-bold mb-2 font-monda">{title}</h3>
         <p>{description}</p>
       </div>
     </Link>
