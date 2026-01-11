b9b786b65a83b815aa5a112a43074ef99954e8e1
Ada Wong <wong.ada1018@gmail.com>
2026-01-11 22:10:14 +0800

    increase z index for top nav bar

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -2,7 +2,7 @@
 
 export default function TopNav() {
   return (
-    <div className="flex gap-6 my-8 h-12 items-center font-monda sticky top-0 bg-background">
+    <div className="flex gap-6 my-8 h-12 items-center font-monda sticky top-0 bg-background z-1">
       {[
         { label: "Home", href: "/" },
         { label: "Email", href: "mailto:business.adawong@gmail.com" },
