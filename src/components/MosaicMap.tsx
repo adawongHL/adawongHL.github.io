12ea4dc4fcafadb781600bb22a60ad5f771670da
Ada Wong <wong.ada1018@gmail.com>
2026-01-12 12:38:08 +0800

    disable mosaic map code

--- src/components/MosaicMap.tsx
+++ src/components/MosaicMap.tsx
@@ -9,62 +9,64 @@ type MapProps = {
 
 
 export default function MosaicMap  ({ width, height, data }: MapProps) {
-  console.log("*** Mosaic Map loaded ***")
+  return <></>
+//   console.log("*** Mosaic Map loaded ***")
 
-  // data = data.features.filter(shape => shape.id === 'AUS' )
-  const projection = d3
-    .geoMercator()
-    .scale(width / (2*Math.PI))
-    .center([90, 50])
-    .translate([515,250]);
+//   // data = data.features.filter(shape => shape.id === 'AUS' )
+//   const projection = d3
+//     .geoMercator()
+//     .scale(width / (2*Math.PI))
+//     .center([90, 50])
+//     .translate([515,250]);
 
-  const cellSize = 1;
-  const spacing = 5;
-  const points: { x: number; y: number }[] = []; // array of points to draw squares for
-  const path = d3.geoPath(projection);
+//   const cellSize = 1;
+//   const spacing = 5;
+//   const points: { x: number; y: number }[] = []; // array of points to draw squares for
+//   const path = d3.geoPath(projection);
 
-  // loop through countries
-  for (let i = 0; i < data.features.length; i++) {
-    // find bounding box for each country
-    const [[x0, y0], [x1, y1]] = path.bounds(data.features[i]);
-    // loop through each point in bounding box
-    for (let y = y0; y < y1; y += spacing) {
-      for (let x = x0; x < x1; x += spacing) {
-        const geoPoint = projection.invert([x,y]); //  convert this point to longitude, latitude
-        if (!geoPoint) continue;
+//   // loop through countries
+//   for (let i = 0; i < data.features.length; i++) {
+//     // find bounding box for each country
+//     const [[x0, y0], [x1, y1]] = path.bounds(data.features[i]);
+//     // loop through each point in bounding box
+//     for (let y = y0; y < y1; y += spacing) {
+//       for (let x = x0; x < x1; x += spacing) {
+//         if (!projection) continue;
+//         const geoPoint = projection.invert([x, y]); // convert this point to longitude, latitude
+//         if (!geoPoint) continue;
 
-        // if point lies within land area, include this point
-        if (d3.geoContains(data.features[i], geoPoint)) {
-          points.push({ x, y });
-        }
-      }
-    }
-  }
+//         // if point lies within land area, include this point
+//         if (d3.geoContains(data.features[i], geoPoint)) {
+//           points.push({ x, y });
+//         }
+//       }
+//     }
+//   }
 
-  // now, points contains all the points to draw squares for
-  // iterate over each point, draw the tiny square
-  const tiles = points.map( (point, index) => 
-    <circle
-    key={index}
-    cx={point.x}
-    cy={point.y}
-    r={1}
-    fill="black"
-  />
-    // <rect 
-    // key={index}
-    // x={point.x}
-    // y={point.y}
-    // width={cellSize}
-    // height={cellSize}
-    // fill="red" />
-  )
+//   // now, points contains all the points to draw squares for
+//   // iterate over each point, draw the tiny square
+//   const tiles = points.map( (point, index) => 
+//     <circle
+//     key={index}
+//     cx={point.x}
+//     cy={point.y}
+//     r={1}
+//     fill="black"
+//   />
+//     // <rect 
+//     // key={index}
+//     // x={point.x}
+//     // y={point.y}
+//     // width={cellSize}
+//     // height={cellSize}
+//     // fill="red" />
+//   )
 
-  return (
-    <div className="bg-red-100">
-    <svg width={width} height={height} className="">
-  {tiles}
-</svg></div>
-  )
+//   return (
+//     <div className="bg-red-100">
+//     <svg width={width} height={height} className="">
+//   {tiles}
+// </svg></div>
+//   )
 
 };
