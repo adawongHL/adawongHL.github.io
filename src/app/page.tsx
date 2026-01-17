e39ec37c1a0860a2e7b8ae3ed62c48ac19e0edeb
Ada Wong <wong.ada1018@gmail.com>
2026-01-17 11:50:18 +0800

    refactor: update colors on home pg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function HomePage() {
       {/* Highlights Section (To Curate) */}
       <section id="highlights" className="mb-16 w-full">
         <div className="flex relative inline-flex items-start">
-          <span className="h-9 w-2 bg-[var(--yellow)] mr-4 bg-[var(--yellow)]" />
+          <span className="h-9 w-2 bg-[var(--yellow)] mr-4 bg-secondary" />
           <h2 className="text-3xl font-bold font-monda mb-6">Projects</h2>
         </div>
         {/* getAlProjects() (DONE) --> create a ProjectCard for each project */}
@@ -53,7 +53,7 @@ export default function HomePage() {
       <section id="blog" className="mb-16 w-full">
         {/* <h2 className="text-3xl font-bold font-monda mb-6">Blog</h2> */}
         <div className="flex relative inline-flex items-start">
-          <span className="h-9 w-2 bg-[var(--yellow)] mr-4 bg-[var(--yellow)]" />
+          <span className="h-9 w-2 bg-[var(--yellow)] mr-4 bg-secondary" />
           <h2 className="text-3xl font-bold font-monda mb-6">Blog</h2>
         </div>
         { blogPosts.length > 0 ? 
