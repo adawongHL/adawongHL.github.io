510af1ec571e1ab968e0834491cc3868cb7379a2
Ada Wong <wong.ada1018@gmail.com>
2026-01-18 15:13:26 +0800

    fix: fade-in animation issue from silent hydration error (use client and next/font tgt)

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,28 +1,14 @@
-"use client";
-import { useState, useEffect } from "react";
 import { monda } from "@/utils/fonts";
 
 export default function Header() {
-  const [fadeIn, setFadeIn] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setFadeIn(true), 100); // delay 100ms before starting animation
-    return () => clearTimeout(timer); 
-  }, []);   // run once upon intial render
-
   return (
-    <div className="flex">
-      <header className={`mb-16 text-left ${monda.className} w-full`}>
-        <h1
-          className={`text-5xl font-bold mb-4 transition-opacity duration-500 ${fadeIn ? "opacity-100" : "opacity-0"}`}
-          style={{ transition: 'opacity 0.3s ease-in' }}>
-          Hi, I'm Ada!
-        </h1>
-        <div className="flex items-end relative">
-          <p className={`text-lg text-gray-600 w-full ${fadeIn ? "opacity-100" : "opacity-0"}`}
-          style={{ transition: 'opacity 0.9s ease-in' }}>I tinker, build and write 🏔️ </p>
-        </div>
-      </header>
-    </div>
+    <header className={`mb-16 text-left ${monda.className} w-full animate-fade-in`}>
+      <h1 className="text-5xl font-bold mb-4">
+        Hi, I'm Ada!
+      </h1>
+      <p className="text-lg text-gray-600">
+        I tinker, build and write 🏔️
+      </p>
+    </header>
   );
 }
